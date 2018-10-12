584754e29159a1c1474d8d0e27cf13cf7b6f8439
Bryant Hinton <bryanthinton01@gmail.com>
2018-10-11 18:52:14 -0600

    Finished home page

--- App.js
+++ App.js
@@ -2,26 +2,11 @@ import React from 'react';
 import {VirtualizedList, Image, ScrollView, FlatList, ActivityIndicator, Button, StyleSheet, Text, View, StatusBar } from 'react-native';
 import { LinearGradient } from 'expo';
 
-<<<<<<< HEAD
-// async function getPoster(title) {
-//   var url = 'http://www.omdbapi.com/?apikey=';
-//   var apikey = '8330622';
-//   title = title.split(' ').join('+');
-//   url +=apikey+'&t='+title; 
-//   return await fetch(url)
-//   .then((response) => response.json())
-//   .then((responseJson) => {
-//     return responseJson.Poster
-// })
-//   .catch((error) => {
-//     console.error(error);
-//     return 'http://www.piniswiss.com/wp-content/uploads/2013/05/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef-300x199.png'
-// });
-// }
-
 global.filters = {'plex' : true, 'hulu' : true, 'netflix' : false, 'amazon' : false}
+global.continueWatching = ["Avengers: Infinity War", "The Incredibles"]
+global.yourList = ["Cars", "Jurassic World", "Coco", "The Hunger Games", "Aladdin", "Batman Begins"]
 
-function concatServices(responseJson){
+function concatServices(responseJson) {
     var currentMovies = []
     var plex = []
     var hulu = []
@@ -53,160 +38,192 @@ function concatServices(responseJson){
     });
 
     currentMovies.forEach(function(i) {
-        console.log(i)
         returnArray.push(responseJson[i])
     });
 
     return returnArray
 }
 
-export default class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {isLoading: true}
+function getContinue(responseJson) {
+    var returnArray = []
+    for (i in responseJson) {
+        for (y in global.continueWatching) {
+            if (responseJson[i].Title == (global.continueWatching)[y])
+                returnArray.push(responseJson[i])
+        }
+    }
+    return returnArray
 }
 
-componentDidMount(){
-    return fetch('https://codegarage.org/plex/allmovies.json')
-    .then((response) => response.json())
-    .then((responseJson) => {
-      
-        var currentMovies = concatServices(responseJson)
-        this.setState({
-            isLoading: false,
-            currentMovies: currentMovies
-        }, 
-        function(responseJson){
-        });
-=======
+function getYourList(responseJson) {
+    var returnArray = []
+    for (i in responseJson) {
+        for (y in global.yourList) {
+            if (responseJson[i].Title == (global.yourList)[y])
+                returnArray.push(responseJson[i])
+        }
+    }
+    return returnArray
+}
+
 export default class App extends React.Component {
-static navigationOptions = {
+
+    static navigationOptions = {
         header: null
     }
-constructor(props){
-  super(props);
-  this.state = {isLoading: true}
-}
 
-componentDidMount(){
-  return fetch('https://codegarage.org/plex/allmovies.json')
-  .then((response) => response.json())
-  .then((responseJson) => {
-    this.setState({
-      isLoading: false,
-      dataSource: responseJson,
-    }, function(responseJson){
-    });
->>>>>>> 834074c960de30c26bfcdafe134b9395b0830ae2
-  })
-    .catch((error) => {
-      console.error(error);
-  });
-}
-
-
-
-render() {
-    if (this.state.isLoading){
-        return(
-            <View style={{flex: 1, padding: 50}}>
-            <ActivityIndicator/>
-            </View>
-        )
+    constructor(props){
+        super(props);
+        this.state = {isLoading: true}
     }
-    return (
-<<<<<<< HEAD
-        <View style={styles.container}>
 
-            <LinearGradient
-                colors={['rgb(32,56,100)','rgb(49,88,157)','rgb(54,96,171)','rgb(53,95,169)']}
-                style={{
-                    position: 'absolute',
-                    left:0,
-                    right:0,
-                    bottom:0,
-                    top:0
-                }}>
+    componentDidMount(){
+        return fetch('https://codegarage.org/plex/allmovies.json')
+        .then((response) => response.json())
+        .then((responseJson) => {
 
-                <VirtualizedList
-                    style={{flex:1, paddingTop: 50, paddingLeft: 2, paddingRight: 2}}
-                    horizontal={true}
-                    maxToRenderPerBatch={4}
-                    data = {this.state.currentMovies}
-                    getItemCount={(data) => {
-                        return 401
-                    }}
-                    getItem={(data, index) => {
-                        return data[index]
-                    }}
-                    keyExtractor={(item, index) =>{
-                        return item.Title
-                    }}
-                    initialNumToRender = { 4 }
-                    updateCellsBatchingPeriod = { 1 }
-                    windowSize={4}
-                    renderItem = {({item}) => {
-                        return (
-                            <View style={{height:200, margin: 10}}>
-                                <Image
-                                    style={{width: 120, height: 180}}
-                                    source={{uri: item.Poster}}
-                                    />
-                                <Text style={{color: 'white', width: 120, marginTop: 5}}>{item.Title}</Text>
-                            </View>
-                        )
-                    }}
-                />
-            </LinearGradient>
-        </View>
-=======
+            var currentMovies = concatServices(responseJson)
+            var continueWatching = getContinue(responseJson)
+            var yourList = getYourList(responseJson)
+            this.setState({
+                isLoading: false,
+                currentMovies: currentMovies,
+                continueWatching: continueWatching,
+                yourList: yourList
+            }, 
+            function(responseJson){
+            });
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+    }
 
-      <View style={styles.container}>
-                  <StatusBar hidden />
-
-        <LinearGradient
-          colors={['rgb(32,56,100)','rgb(49,88,157)','rgb(54,96,171)','rgb(53,95,169)']}
-          style={{
-            position: 'absolute',
-            left:0,
-            right:0,
-            bottom:0,
-            top:0
-          }} >
-
-          <VirtualizedList
-           style={{flex:1, paddingTop: 50, paddingLeft: 2, paddingRight: 2}}
-           horizontal={true}
-           maxToRenderPerBatch={4}
-           data = {this.state.dataSource}
-           getItemCount={(data) => {
-            return 401
-          }}
-           getItem={(data, index) => {
-            return data[index]
-          }}
-           keyExtractor={(item, index) =>{
-            return item.Title
-           }}
-           initialNumToRender = { 4 }
-           updateCellsBatchingPeriod = { 1 }
-           windowSize={4}
-           renderItem = {({item}) => {
-              return (
-                <View style={{height:50}}>
-                
-                <Text style={{color: 'white'}}>{item.Title}</Text>
+    render() {
+        if (this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 50}}>
+                <ActivityIndicator/>
                 </View>
-              )
-            }
-           }
-           />
-        </LinearGradient>
-      </View>
->>>>>>> 834074c960de30c26bfcdafe134b9395b0830ae2
-    );
-}
+                )
+        }
+        return (
+            // <View style={styles.container}>
+            
+            <LinearGradient
+            colors={['rgb(32,56,100)','rgb(49,88,157)','rgb(54,96,171)','rgb(53,95,169)']}
+            style={{
+                position: 'absolute',
+                left:0,
+                right:0,
+                bottom:0,
+                top:0
+            }}>
+            <ScrollView>
+
+
+            <Text style={{paddingTop: 50, paddingLeft: 10, color: 'white', fontSize: 25}}>Continue Watching</Text>
+            <VirtualizedList
+            style={{flex:1, paddingTop: 5, margin: 0, paddingLeft: 10}}
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            maxToRenderPerBatch={4}
+            data = {this.state.continueWatching}
+            getItemCount={(data) => {
+                return data.length
+            }}
+            getItem={(data, index) => {
+                return data[index]
+            }}
+            keyExtractor={(item, index) =>{
+                return item.Title
+            }}
+            initialNumToRender = { 4 }
+            updateCellsBatchingPeriod = { 1 }
+            windowSize={4}
+            renderItem = {({item}) => {
+                return (
+                    <View style={{margin: 10, marginBottom: 0}}>
+                    <Image
+                    style={{width: 120, height: 180}}
+                    source={{uri: item.Poster}}
+                    />
+                    <Text style={{color: 'white', width: 120, marginTop: 5}}>{item.Title}</Text>
+                    </View>
+                    )
+            }}
+            />
+
+
+            <Text style={{paddingTop: 30, paddingLeft: 10, color: 'white', fontSize: 25}}>Your List</Text>
+            <VirtualizedList
+            style={{flex:1, paddingTop: 5, margin: 0, paddingLeft: 10}}
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            maxToRenderPerBatch={4}
+            data = {this.state.yourList}
+            getItemCount={(data) => {
+                return data.length
+            }}
+            getItem={(data, index) => {
+                return data[index]
+            }}
+            keyExtractor={(item, index) =>{
+                return item.Title
+            }}
+            initialNumToRender = { 4 }
+            updateCellsBatchingPeriod = { 1 }
+            windowSize={4}
+            renderItem = {({item}) => {
+                return (
+                    <View style={{margin: 10}}>
+                    <Image
+                    style={{width: 120, height: 180}}
+                    source={{uri: item.Poster}}
+                    />
+                    <Text style={{color: 'white', width: 120, marginTop: 5}}>{item.Title}</Text>
+                    </View>
+                    )
+            }}
+            />
+            
+
+            <Text style={{paddingTop: 30, paddingLeft: 10, color: 'white', fontSize: 25}}>For You</Text>
+            <VirtualizedList
+            style={{flex:1, paddingTop: 5, margin: 0, paddingLeft: 10}}
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            maxToRenderPerBatch={4}
+            data = {this.state.currentMovies}
+            getItemCount={(data) => {
+                return data.length
+            }}
+            getItem={(data, index) => {
+                return data[index]
+            }}
+            keyExtractor={(item, index) =>{
+                return item.Title
+            }}
+            initialNumToRender = { 4 }
+            updateCellsBatchingPeriod = { 1 }
+            windowSize={4}
+            renderItem = {({item}) => {
+                return (
+                    <View style={{height:200, margin: 10, marginBottom: 40}}>
+                    <Image
+                    style={{width: 120, height: 180}}
+                    source={{uri: item.Poster}}
+                    />
+                    <Text style={{color: 'white', width: 120, marginTop: 5}}>{item.Title}</Text>
+                    </View>
+                    )
+            }}
+            />
+            </ScrollView>
+            </LinearGradient>
+            
+            );
+    }
 }
 
 const styles = StyleSheet.create({
